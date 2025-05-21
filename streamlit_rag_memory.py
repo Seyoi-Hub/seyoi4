651356ac6cd9e2bd84f22eb6c176dc3ceed9ab36
Seyoi-Hub <elisack00@naver.com>
2025-05-21 10:54:42 +0900

    Add files via upload

---
+++ streamlit_rag_memory.py
@@ -0,0 +1,15 @@
+import os
+import streamlit as st
+import tempfile
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
+
+
+os.environ["OPENAI_API_KEY"]=st.secrets['OPENAI_API_KEY']
